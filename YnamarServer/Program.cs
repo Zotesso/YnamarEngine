4c417768622e3f33070bf22ca7e511fcda8b1026
Pedro Zotesso <siriothart@gmail.com>
2024-07-02 22:48:31 -0300

    Adicionar arquivos de projeto.

---
+++ YnamarServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YnamarServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
